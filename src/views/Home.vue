741eb241a673f93f42c6849d122aaf99a5afce09
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-02-18 22:40:54 +0530

    Restructred the layout | Common component can now show up for all the page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,5 @@ import { Collaborate, FAQ } from '@/components';
 </script>
 
 <template>
-	<section class="section-margin-top-30-mobile">
-		<!-- FAQ Section Begins -->
-		<FAQ />
-	</section>
-	<!-- FAQ Section Ends -->
-	<section class="section-margin-top-60 section-margin-top-90-mobile">
-		<!-- Collaborate Section Begins -->
-		<Collaborate />
-	</section>
-	<!-- Collaborate Section Ends -->
+	
 </template>
