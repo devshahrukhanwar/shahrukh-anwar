957d1254fcd45c4abc0d37b82c3d31372fa12f9d
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-02-24 20:48:58 +0530

    Added vercel.json to tackle with redirecting issue on server | Routes should redirect to their respective views otherwise on Home now

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [
     vue({
       script: {
