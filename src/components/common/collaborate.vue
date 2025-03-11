277e3fa0d93434d135b2bcc27a23eb6eaffd14c7
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-03-12 00:09:46 +0530

    IconWrapper compnent added for using icons inside a dot dynamically

--- src/components/common/collaborate.vue
+++ src/components/common/collaborate.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { IconWrapper } from '@/components';
+
+const isHovered = ref(false);
+</script>
 
 <template>
 	<router-link :to="{ name: 'Contact' }">
 		<div
 			class="collaborate container card p-5 has-text-left is-clickable"
+			@mouseenter="isHovered = true"
+			@mouseleave="isHovered = false"
 		>
 			<h2>
 				Let’s
@@ -15,10 +22,7 @@
 				services. Let’s collaborate to create user-centered solutions that not
 				only meet your goals but also delight your users.
 			</p>
-			<div class="arrow">
-				<i class="fa-solid fa-arrow-up-long first"></i>
-				<i class="fa-solid fa-arrow-up-long second"></i>
-			</div>
+			<IconWrapper icon="arrow" :tilted="true" :hovered="isHovered" />
 		</div>
 	</router-link>
 </template>
@@ -33,35 +37,6 @@
 		color: var(--text-color-grey);
 		line-height: 1.2em;
 	}
-	.arrow {
-		position: absolute;
-		top: 8%;
-		right: 3%;
-		padding: 25px;
-		display: inline-block;
-		border-radius: 30px;
-		background-color: var(--bg-purple);
-
-		i {
-			top: 28%;
-			right: 31%;
-			font-size: 24px;
-			position: absolute;
-			transform: rotate(45deg);
-			transition:
-				transform 0.3s ease,
-				color 0.3s ease;
-
-			&.first {
-				transform: translateY(45deg);
-			}
-			&.second {
-				transform: translateY(45deg);
-				opacity: 0;
-				transition: opacity 0.3s ease;
-			}
-		}
-	}
 
 	&:hover {
 		background-color: var(--bg-purple);
@@ -69,29 +44,11 @@
 		.text-highlight {
 			color: var(--text-color);
 		}
-		.arrow {
-			background-color: var(--bg-white);
-
-			i {
-				color: var(--text-highlight);
-
-				&.first {
-					opacity: 0;
-				}
-				&.second {
-					opacity: 1;
-				}
-			}
-		}
 	}
 
 	@media screen and (max-width: 768px) {
 		h2 {
 			font-size: 38px;
-		}
-		.arrow {
-			top: 10%;
-			right: 6%;
 		}
 	}
 }
