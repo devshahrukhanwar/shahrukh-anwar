5d93fe43eabb03ae06787b7cca903cf2679a64af
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-03-12 00:10:43 +0530

    Icons collection added to use the icons easily with single name

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,6 @@ import experience from './experience.json';
 import faq from './faq.json';
 import profile from './profile.json';
 import socials from './socials.json';
+import icons from './icons.json';
 
-export { profile, socials, faq, experience };
+export { profile, socials, faq, experience, icons };
