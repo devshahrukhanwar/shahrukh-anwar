4e1b259e4b7cd64f2762547bf10321a2d1b76c64
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-03-14 22:19:36 +0530

    Added the tools to the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
 import { Introduction } from '@/components';
+import { Tools } from '@/views';
 </script>
 
 <template>
 	<section>
 		<Introduction />
+		<Tools />
 	</section>
 </template>
+
+<style lang="scss" scoped>
+::v-deep(.tools) {
+	margin-top: 3rem;
+}
+::v-deep(.tools .headline .heading) {
+	font-size: 58px !important;
+	font-weight: 500 !important;
+	text-shadow: 0.03em 0 0 currentColor;
+
+	@media screen and (max-width: 768px) {
+		font-size: var(--h2-mobile-size) !important;
+	}
+}
+
+@media screen and (max-width: 768px) {
+	::v-deep(.tools) {
+		margin-top: unset;
+	}
+}
+</style>
+
