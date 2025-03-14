234bed8c6e2768c0f1fabbc8173f9538050210d0
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-03-14 22:18:10 +0530

    Tools config added for the tools of expertise

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,6 @@ import faq from './faq.json';
 import profile from './profile.json';
 import socials from './socials.json';
 import icons from './icons.json';
+import tools from './tools.json'
 
-export { profile, socials, faq, experience, icons };
+export { profile, socials, faq, experience, icons, tools };
