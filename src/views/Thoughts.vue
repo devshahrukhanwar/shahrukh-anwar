5e5a0d389ba67ffa87c3f526ea66de56c21cfae6
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-21 17:24:06 +0530

    Added the thoughts tab in the nav bar

--- src/views/Thoughts.vue
+++ src/views/Thoughts.vue
@@ -1,3 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Card } from '@/components';
+import { thoughts } from '@/config';
+import { useSocial } from '@/composables';
 
-<template></template>
+const { getSocialData } = useSocial();
+
+const socialData = getSocialData();
+</script>
+
+<template>
+	<div class="columns is-block thoughts">
+		<div class="column has-text-left has-text-centered-mobile pb-5 headline">
+			<p class="heading">
+				My Thoughts<span class="is-hidden-desktop"></span>
+        <br />
+        <span> and </span>
+        <span class="text-highlight">Perspectives</span>
+			</p>
+		</div>
+		<div class="column is-flex-desktop is-flex-wrap-wrap p-0 pl-3">
+			<div class="column is-6 pl-0" v-for="thought in thoughts" :key="thought.title">
+				<Card :data="thought" :isLink="false" />
+			</div>
+		</div>
+	</div>
+</template>
