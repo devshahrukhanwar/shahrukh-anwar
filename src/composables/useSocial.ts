6d15fd796f6a1abb1afe21ccf26f30d2e5f62aba
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-22 03:14:06 +0530

    Used "zod" for the safe parsing of schema from the social post API

--- src/composables/useSocial.ts
+++ src/composables/useSocial.ts
@@ -1,14 +1,17 @@
 import axios from 'axios';
 
+import { type TwitterSchema, twitterSchema } from '@/stores/thoughts/schema';
+
 export function useSocial() {
-	async function getSocialData(): Promise<void> {
+	async function getSocialData(): Promise<TwitterSchema> {
 		try {
 			const response = await axios.get(
 				`${import.meta.env.VITE_API_URL}/social/posts`
 			);
 
-			console.log('Social data fetched successfully:', response.data);
-			return response.data;
+			const { data } = response.data;
+
+			return twitterSchema.parse(data);
 		} catch (error) {
 			console.error('Error fetching social data:', error);
 		}
