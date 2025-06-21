7e851e0fd1171434bdaef8f5ce600a2bcaa89437
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-22 03:15:51 +0530

    Updated the Thoughts page to include Twitter card and my blogs section from the API

--- src/views/Thoughts.vue
+++ src/views/Thoughts.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import { Card, TwitterCard } from '@/components';
-import { thoughts } from '@/config';
+import { type TwitterSchema } from '@/stores/thoughts/schema'
+import { Card, Carousel, TwitterCard } from '@/components';
+import { thoughts, socials } from '@/config';
 import { useSocial } from '@/composables';
-
-const { getSocialData } = useSocial();
-
 import { ref, onMounted } from 'vue';
 
-// const socialData = ref(null);
+const { getSocialData } = useSocial();
+const socialData = ref<TwitterSchema | null>(null);
 
-// onMounted(async () => {
-// 	socialData.value = await getSocialData();
-// });
-// console.log('==========> socialData', socialData);
+const twitterURL = socials.twitter.url;
+
+onMounted(async () => {
+	socialData.value = await getSocialData();
+});
 </script>
 
 <template>
-	<div class="columns is-block thoughts">
+	<div class="thoughts columns container is-block">
 		<div class="column has-text-left has-text-centered-mobile pb-5 headline">
 			<p class="heading">
 				My Thoughts<span class="is-hidden-desktop"></span>
@@ -27,16 +27,34 @@ import { ref, onMounted } from 'vue';
 		</div>
 		<div class="column p-0">
 			<div class="twitter-section column has-text-left">
-				<div class="bullets column px-0">
-					<span class="text-highlight is-clickable">#</span> Twitter Feed
+				<div class="bullets column is-flex p-0 pb-3">
+					<div class="column p-0">
+						<span class="text-highlight is-clickable">#</span> My Thoughts
+					</div>
 				</div>
 				<div class="column is-flex">
-					<TwitterCard />
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						:href="twitterURL"
+					>
+						<TwitterCard
+							:tweets="socialData.twitter.tweets"
+							:user="socialData.twitter.user"
+							v-if="socialData" />
+					</a>
 				</div>
 			</div>
-			<div class="blog-section column is-flex-desktop is-flex-wrap-wrap">
-				<div class="column is-6 pl-0" v-for="thought in thoughts" :key="thought.title">
-					<Card :data="thought" :isLink="false" />
+			<div class="blog-section column has-text-left mt-5">
+				<div class="bullets column is-flex p-0 pb-3">
+					<div class="column p-0">
+						<span class="text-highlight is-clickable">#</span> My Blogs
+					</div>
+				</div>
+				<div class="column is-flex-desktop is-flex-wrap-wrap p-0">
+					<div class="column is-6 pl-0" v-for="thought in thoughts" :key="thought.title">
+						<Card :data="thought" :isLink="false" />
+					</div>
 				</div>
 			</div>
 		</div>
@@ -50,12 +68,8 @@ import { ref, onMounted } from 'vue';
 	}
 
 	@media screen and (max-width: 768px) {
-    .twitter-section {
+    .twitter-section, .column.is-flex, .blog-section {
 			padding-right: 0;
-
-			.column.is-flex {
-				padding-right: 0;
-			}
 		}
   }
 }
