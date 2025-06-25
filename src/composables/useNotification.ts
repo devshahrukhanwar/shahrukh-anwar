8d211b2a2acf64c7d94e0afa5e3cbbfaaacf6c6f
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-26 03:46:59 +0530

    Removed the notifications code from the frontend and using the '/notifications' endpoint to send them

--- src/composables/useNotification.ts
+++ src/composables/useNotification.ts
@@ -1,40 +1,34 @@
-import { getHTML, telegram } from '@/utils'
-import { ThankYou, EmailNotification } from '@/components'
-import { type Content } from "@/notifications/NotificationSender"
-import { Notification, EmailSender, TelegramSender, type Contact } from "@/notifications"
+import axios from 'axios';
+
+export interface Contact {
+	name: string;
+	email: string;
+	message?: string;
+}
 
 export function useNotification() {
-  async function sendNotification(contact: Contact): Promise<void> {
-    const { TGContent, ThanksContent, ContactContent } = getContent(contact)
+	async function sendNotification(contact: Contact): Promise<void> {
+		try {
+			const formData = new FormData();
+			Object.entries(contact).forEach(([key, value]) => {
+				if (value !== undefined && value !== null) {
+					formData.append(key, value);
+				}
+			});
 
-    const notifyEmail = new Notification(new EmailSender())
-    const notifyTG = new Notification(new TelegramSender())
-    const notifyThanks = new Notification((new EmailSender()).setTo([contact]))
+			await axios.post(
+				`${import.meta.env.VITE_API_URL}/notifications`,
+				formData,
+				{
+					headers: {
+						'Content-Type': 'multipart/form-data'
+					}
+				}
+			);
+		} catch (error) {
+			console.error('Error fetching social data:', error);
+		}
+	}
 
-    Promise.race([
-      notifyThanks.send(contact, ThanksContent),
-      notifyEmail.send(contact, ContactContent),
-      notifyTG.send(contact, TGContent)
-    ])
-  }
-
-  function getContent(contact: Contact) {
-    const TGContent: Content = {
-      text: <string>telegram.contact(contact).trim()
-    }
-
-    const ThanksContent: Content = {
-      subject: <string>(`Thank you for contacting me, ${contact.name}`),
-      text: <string>getHTML(ThankYou, contact).trim()
-    }
-
-    const ContactContent: Content = {
-      subject: <string>(`Portfolio | Contact Form Submission from ${contact.name}`),
-      text: <string>getHTML(EmailNotification, contact).trim()
-    }
-
-    return { TGContent, ThanksContent, ContactContent }
-  }
-
-  return { sendNotification }
+	return { sendNotification };
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
 import { formattedText, getDate, getHTML, totalExperience } from './formatters';
-import { telegram } from './telegram';
 
-export { getHTML, getDate, formattedText, totalExperience, telegram };
+export { getHTML, getDate, formattedText, totalExperience };
