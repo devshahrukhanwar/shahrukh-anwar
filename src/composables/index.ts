c1cf87b779e4dbbb9a5df1d7e6e917c107ce9ae3
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-06-27 01:33:17 +0530

    Added the vercel analytics to the project

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,4 +1,13 @@
+import { useAnalytics } from './useAnalytics';
 import { type Contact, useNotification } from './useNotification';
 import { useSocial } from './useSocial';
+import { type AllowedPropertyValues, useVercel } from './useVercel';
 
-export { useSocial, useNotification, type Contact };
+export {
+	useSocial,
+	useNotification,
+	type Contact,
+	useAnalytics,
+	useVercel,
+	type AllowedPropertyValues
+};
