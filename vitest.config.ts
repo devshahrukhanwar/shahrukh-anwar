4e551afe7f4f1675b0bdff4cdbab8f7640fdcc18
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-07-04 22:45:28 +0530

    Added the vitest related config files

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src')
+		}
+	},
+	test: {
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: ['./vitest.setup.ts'],
+		include: ['src/**/*.spec.ts'],
+		coverage: {
+			reporter: ['text', 'json', 'html']
+		}
+	}
+});
