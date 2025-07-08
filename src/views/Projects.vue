723b5199d226be92134b18e2651421428c4e8c5f
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-07-09 00:23:27 +0530

    GitHub pinned repos added to showcase in the projects page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { Card } from '@/components';
-import { projects } from '@/config';
+import { misc, projects } from '@/config';
+import { type GitHubSchema } from '@/stores/misc/schema';
+
+const hoveredRepo = ref<string | null>(null);
+const repositories: GitHubSchema[] = misc.github.repositories;
 </script>
 
 <template>
@@ -13,15 +18,50 @@ import { projects } from '@/config';
 				<span class="text-highlight">Achievements</span>
 			</p>
 		</div>
-		<div class="column is-flex-desktop is-flex-wrap-wrap p-0 pl-3">
-			<div
-				class="column is-6 pl-0"
-				v-for="project in projects"
-				:key="project.title"
-			>
-				<a :href="project.url" target="_blank" rel="noopener noreferrer">
-					<Card :data="project" :isLink="true" :tags="true" />
-				</a>
+		<div class="column p-0 pl-3">
+			<div class="my-projects column is-block p-0">
+				<div class="bullets column has-text-left mb-1 p-0">
+					<span class="text-highlight is-clickable"># </span>My Projects
+				</div>
+				<div class="column is-flex-desktop is-flex-wrap-wrap p-0">
+					<div
+						class="column is-6 pl-0 pb-0"
+						v-for="repo in repositories"
+						:key="repo.url"
+					>
+						<a :href="repo.url" target="_blank" rel="noopener noreferrer">
+							<div class="column is-narrow p-0 is-clickable">
+								<div class="column is-narrow is-align-items-center p-0">
+									<div class="column banner p-0">
+										<img
+											:class="{'is-hovered': hoveredRepo === repo.url}"
+											:src="repo.banner"
+											:alt="repo.url"
+											@mouseover="hoveredRepo = repo.url"
+											@mouseleave="hoveredRepo = null"
+										/>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="client-projects column is-block mt-5 p-0">
+				<div class="bullets column has-text-left mb-1 p-0">
+					<span class="text-highlight is-clickable"># </span>Client Projects
+				</div>
+				<div class="column is-flex-desktop is-flex-wrap-wrap p-0">
+					<div
+						class="column is-flex-desktop is-6 pl-0"
+						v-for="project in projects"
+						:key="project.title"
+					>
+						<a :href="project.url" target="_blank" rel="noopener noreferrer">
+							<Card :data="project" :isLink="true" :tags="true" />
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -29,6 +69,29 @@ import { projects } from '@/config';
 
 <style lang="scss" scoped>
 .projects {
+	.bullets {
+		font-size: 20px;
+	}
+
+	.my-projects {
+		.banner {
+			img {
+				border-radius: 0.75rem;
+				transition: transform 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+			}
+		}
+	}
+	
+	&:hover {
+		.my-projects {
+			.banner {
+				.is-hovered {
+					transform: scaleX(1.01) scaleY(1.01);
+				}
+			}
+		}
+	}
+
 	::v-deep(.card .subtitle) {
 		margin-bottom: 10px;
 	}
